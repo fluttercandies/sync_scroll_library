5ffe6bee30e1b4f50dadb92e7fbf8dd8752e2f54
zmtzawqlp <zmtzawqlp@live.com>
2022-10-11 21:13:55 +0800

    fix unnecessary import

--- lib/src/scroll_physics.dart
+++ lib/src/scroll_physics.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/physics.dart';
+import 'package:flutter/physics.dart' as physics;
 
 class NeverScrollableClampingScrollPhysics extends ClampingScrollPhysics
     with LessSpringScrollPhysics {
@@ -10,7 +10,8 @@ class NeverScrollableClampingScrollPhysics extends ClampingScrollPhysics
 /// reduce animation time
 mixin LessSpringScrollPhysics on ScrollPhysics {
   @override
-  SpringDescription get spring => SpringDescription.withDampingRatio(
+  physics.SpringDescription get spring =>
+      physics.SpringDescription.withDampingRatio(
         mass: 0.5,
         stiffness: 1000.0, // Increase this value as you wish.
         ratio: 1.1,
